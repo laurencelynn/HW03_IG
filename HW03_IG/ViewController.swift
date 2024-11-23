980808af40ced83fc3bf52b3120f302240d576a6
LaurenceLin <laurencelin0614@icloud.com>
2024-11-23 20:40:33 +0800

    Initial Commit

---
+++ HW03_IG/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW03_IG
+//
+//  Created by 林上閔 on 2024/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ HW03_IGTests/HW03_IGTests.swift
@@ -0,0 +1,17 @@
+//
+//  HW03_IGTests.swift
+//  HW03_IGTests
+//
+//  Created by 林上閔 on 2024/11/23.
+//
+
+import Testing
+@testable import HW03_IG
+
+struct HW03_IGTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
